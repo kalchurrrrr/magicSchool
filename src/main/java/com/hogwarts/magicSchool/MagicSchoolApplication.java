7564fd6864e2352fbebd5e4f83d8f7e0a4b4a167
kalchurrrrr <nikolaykalchu@gmail.com>
2023-09-26 09:19:11 +0300

    Initial commit

---
+++ src/main/java/com/hogwarts/magicSchool/MagicSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.hogwarts.magicSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hogwarts/magicSchool/MagicSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogwarts.magicSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
